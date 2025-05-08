c493af90c65bdc5615d572272d083702401b5c73
suzuki <kumanojudoka@gmail.com>
2025-05-08 15:33:38 +0900

    バラの攻撃

--- Assets/Project/Scripts/PrincesAttackController_szk.cs
+++ Assets/Project/Scripts/PrincesAttackController_szk.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEditor.Callbacks;
 
 public class PrincesAttackController_szk : MonoBehaviour
 {
     public float speed = 10f;
+    Rigidbody2D rb;
 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
     public void Attack(Vector3 direction){
-        transform.DOMove(transform.position + direction * speed ,1f)
-        .SetEase(Ease.OutQuad).OnComplete(()=>Destroy(gameObject));
+        rb.AddForce(direction * 10f ,ForceMode2D.Impulse);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyBase eneCon = other.collider.GetComponent<EnemyBase>();
+        Destroy(gameObject);
     }
 
 }
