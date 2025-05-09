926bbeb2bf01fd40504b3c4f23dbf43537ba4281
kou2025 <kou.roppongi@gmail.com>
2025-05-09 13:25:06 +0900

    EnemyBaseのOnTriggerをOncolliderへ修正

--- Assets/Project/Scripts/TurretEnemyController.cs
+++ Assets/Project/Scripts/TurretEnemyController.cs
@@ -8,14 +8,30 @@ public class TurretEnemyController : EnemyBase
     public Transform firePoint;
     public float bulletSpeed = 5f;
     public float fireRate = 2f;
+    public float shootRadius = 9f; //攻撃開始範囲
+    private float lastShootTime = 0f;
 
     protected override void Start()
     {
         base.Start();
         canMove = false; //砲台の為、移動させないようにする 
 
-        //定期的に弾発射
-        InvokeRepeating("ShootBullet", 0f, fireRate);
+        // //定期的に弾発射
+        // InvokeRepeating("ShootBullet", 0f, fireRate);
+    }
+
+    protected override void Update()
+    {
+        if(player == null)return;
+    
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= shootRadius && Time.time >= lastShootTime + fireRate)
+        {
+            ShootBullet();
+            lastShootTime = Time.time;
+        }
+
     }
 
     //直接攻撃じゃない為、空のAttackメソッド
@@ -24,11 +40,13 @@ public class TurretEnemyController : EnemyBase
     //弾発射処理
     void ShootBullet()
     {
+
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
 
         Vector2 direction = (player.transform.position - transform.position).normalized;
         bulletRb.velocity = direction * bulletSpeed;
+
 
     }
 }
